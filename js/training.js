619d2b592d90a07c19f9e973cd8310e632352c89
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-04-26 16:56:08 +0200

    its not easy

--- js/training.js
+++ js/training.js
@@ -1,18 +1,45 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
+const textGhost = document.getElementById("text-ghost");
+const button = document.getElementById("btn");
+
+function ghostTheText(event) {
+    textGhost.classList.toggle("ghost-text")
+}
+button.addEventListener('click', ghostTheText);
 
 
+
+
+console.log(textGhost);
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
+const scrollValue = document.getElementById("ex2-scroll-value");
+function scrolling(event){
+    scrollValue.innerText = window.scrollY;
+}
+window.addEventListener('scroll', scrolling)
 
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
+const listOfAnimals = document.querySelector('#ex3-animals');
+const button2 = document.querySelector('#button2');
 
-
+function moveTheAnimals(event) {
+    listOfAnimals.appendChild(listOfAnimals.firstElementChild)
+}
+button2.addEventListener('click', moveTheAnimals)
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
+const buttonForColor = document.querySelector('#buttons')
 
+function clickTheButton(event) {
+    buttonForColor.classList.toggle("blue");
+    buttonForColor.classList.toggle("red");
+    buttonForColor.classList.toggle("green");
+}
+buttonForColor.addEventListener('click', clickTheButton)
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
